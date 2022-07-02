56912bd6a8cfce2361af5782493bb7fe642e8d55
Kirlin Mensah <kirlinmensah@gmail.com>
2022-07-02 13:06:11 -0700

    sorry, please i have added comments now

--- kgdm.py
+++ kgdm.py
@@ -1,4 +1,6 @@
+# creating a class Animalia
 class Animalia:
+    # initiating instance attributes
     def __init__(self, Kingdom, Class, Order, Genus, Species):
         self.Kingdom = Kingdom
         self.Class = Class
@@ -7,20 +9,23 @@ class Animalia:
         self.Species = Species
     
         
-    
+# creating an instance methods    
     def description(self):
         return f"I am from Kingdom {self.Kingdom} and a Specie of the {self.Species} Family"
-
+# another instance method
     def it(self):
         return f"Wow!, wow!, I'm a dog"
 
+# creating a child class, inheriting from the parent class
 class Dog(Animalia):
     pass
-    
+# another child class inheriting from the parent class     
 class Person(Animalia):
+    # an instance method of the child class
     def myself(self):
         return f"Hi, I'm a person, and i can talk"
 
+# creating objects of the classes
 Hope = Animalia("Animalia", "", "", "Homo", "Sapien")
 print(Hope.description())
 print(Dog.it(""))
